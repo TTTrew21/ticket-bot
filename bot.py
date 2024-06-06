d31cf7a76001a35c164fcd2c65017ff5476b5f77
TTTTTT <147995506+TTTrew21@users.noreply.github.com>
2024-06-06 22:48:00 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,8 @@
+from time import sleep as wait
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path='./chromedriver.exe')
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("") # link here
